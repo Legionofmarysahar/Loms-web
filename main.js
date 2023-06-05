1a4a5895bf60e9889bd7c43a501e2bd8237d0bb5
legionofmarysahar@gmail.com <legionofmarysahar@gmail.com>
2023-06-06 02:00:55 +0530

    206jun

--- main.js
+++ main.js
@@ -7,14 +7,44 @@ function myFunction() {
   }
 }
 
-let btn = document.querySelector(".icon");
-        let icon = btn.querySelector(".fa-bars");
+function myFunction2() {
+  var x = document.getElementById("nav-link-box");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
 
-        btn.onclick = function (){
-            if(icon.classList.contains("fa-bars")){
-                icon.classList.replace("fa-bars", "fa-times");
-            }
-            else{
-                icon.classList.replace("fa-times", "fa-bars");
-            }
-        }
+
+/*let btn = document.querySelector(".icon");
+let icon = btn.querySelector(".fa-bars");
+
+btn.onclick = function() {
+  if (icon.classList.contains("fa-bars")) {
+    icon.classList.replace("fa-bars", "fa-times");
+  }
+  else {
+    icon.classList.replace("fa-times", "fa-bars");
+  }
+}*/
+
+// Get the button:
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() { scrollFunction() };
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
